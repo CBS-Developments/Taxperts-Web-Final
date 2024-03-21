c42d7e9f3300cd632862a082ab4723fe87281a6e
Dinethri Gunawardhana <dinethriupeksha200@gmail.com>
2024-03-21 08:57:05 +0530

    vlog add

--- taxperts_web_final/lib/VlogPage/vlog_desktop.dart
+++ taxperts_web_final/lib/VlogPage/vlog_desktop.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../Components/navBar.dart';
+import '../colors.dart';
 
 class VlogDesktop extends StatefulWidget {
   const VlogDesktop({Key? key}) : super(key: key);
@@ -21,44 +22,97 @@ class _VlogDesktopState extends State<VlogDesktop> {
 
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double p30 = screenWidth / 60;
+    double p60 = screenWidth / 30;
+    double p15 = screenWidth / 120;
+    double p120 = screenWidth / 15;
+    double p250 = screenWidth / 6;
     return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            // padding: EdgeInsets.symmetric(vertical: 20),
-            decoration: BoxDecoration(
-              color: Colors.white, // Background color
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(
-                      0.1), // Shadow color with some transparency
-                  spreadRadius: 0, // Spread radius
-                  blurRadius: 4, // Blur radius
-                  offset: Offset(0, 2), // changes position of shadow
-                ),
-              ],
-              // If you also want to round the corners of the container
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              // padding: EdgeInsets.symmetric(vertical: 20),
+              decoration: BoxDecoration(
+                color: Colors.white, // Background color
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(
+                        0.1), // Shadow color with some transparency
+                    spreadRadius: 0, // Spread radius
+                    blurRadius: 4, // Blur radius
+                    offset: const Offset(0, 2), // changes position of shadow
+                  ),
+                ],
+                // If you also want to round the corners of the container
+              ),
+              height:
+              100, // Set the container height to match the AppBar height
+              child: Row(
+                crossAxisAlignment:
+                CrossAxisAlignment.center, // Center the Row vertically
+                mainAxisAlignment:
+                MainAxisAlignment.spaceBetween, // Space items horizontally
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(left: 50.0),
+                    child: Image.asset('images/logo.png', height: 50),
+                  ), // Adjust the size of the logo if necessary
+                  CustomNavigationBar(
+                    selectedIndex: _selectedIndex,
+                    onItemSelected: _navigateTo,
+                  ),
+                ],
+              ),
             ),
-            height:
-            100, // Set the container height to match the AppBar height
-            child: Row(
-              crossAxisAlignment:
-              CrossAxisAlignment.center, // Center the Row vertically
-              mainAxisAlignment:
-              MainAxisAlignment.spaceBetween, // Space items horizontally
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.only(left: 50.0),
-                  child: Image.asset('images/logo.png', height: 50),
-                ), // Adjust the size of the logo if necessary
-                CustomNavigationBar(
-                  selectedIndex: _selectedIndex,
-                  onItemSelected: _navigateTo,
-                ),
-              ],
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: p30),
+              height: 650,
+              width: double.infinity,
+              child: Stack(
+                children: <Widget>[
+                  Positioned.fill(
+                    child: Image.asset(
+                      'images/vlog650.png', // Replace with your background image
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  // Centered Column for text and button overlay
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'VLOG',
+                          style: TextStyle(
+                            fontSize: 60,
+                            color: AppColor.appTeal,
+                            fontWeight: FontWeight.w800,
+                          ),
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 2.0),
+                          child: Text(
+                            '''We believe in doing your taxes right. We’re committed to serving you assuring  \nyour comfort in tax compliance decision. ''',
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors
+                                  .black, // Make sure AppColor class is defined or imported
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 30),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
 
     );
